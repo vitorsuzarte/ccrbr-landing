9d99ec1bebc77f01b5027e413c0d5ef0bc602a49
Vitor Suzarte <vitorvsuzarte@gmail.com>
2023-10-26 21:35:20 -0300

    fixing babel issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Qual foi caiozinho, to te esperando hein
-      </h1>
-      <p>
-        Quando chegar a hora, AAAAAAAAAAAAAA, quando chegar, ai eu troco a url feia por uma massa tipo caiozineo-mlk-doido-psicologo.caio.com
-      </p>
+        <h1>
+          Qual foi caiozinho, to te esperando hein
+        </h1>
+        <p>
+          Quando chegar a hora, AAAAAAAAAAAAAA, quando chegar, ai eu troco a url feia por uma massa tipo caiozineo-mlk-doido-psicologo.caio.com
+        </p>
     </div>
   );
 }
